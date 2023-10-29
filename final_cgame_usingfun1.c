cd29cfc4dd67b024ead79e1c492fe0bd448e07cc
Qwefs <87111130+Qwefs@users.noreply.github.com>
2023-10-29 22:04:39 +0530

    Add files via upload

--- final_cgame_usingfun1.c
+++ final_cgame_usingfun1.c
@@ -3,65 +3,6 @@
 #include <time.h>
 #include <string.h>
 
-
-char* myChoice() {
-    int user_choice;
-    char *userChoice;
-
-    while (1) {
-        printf("Enter your choice:\n1. Rock\n2. Paper\n3. Scissors\n");
-        printf("Your choice: ");
-        scanf("%d", &user_choice);
-
-        if (user_choice >= 1 && user_choice <= 3) {
-            switch (user_choice) {
-                case 1:
-                    userChoice = "rock";
-                    break;
-                case 2:
-                    userChoice = "scissors";
-                    break;
-                case 3:
-                    userChoice = "paper";
-                    break;
-            }
-            return userChoice;
-        } else {
-            printf("Invalid choice. Please choose a number between 1 and 3.\n");
-        }
-    }
-}
-
-
-char* randomChoice() {
-    char *choices[] = {"rock", "paper", "scissors"};
-    int num_choices = 3;
-    int randomIndex = rand() % num_choices;
-    return choices[randomIndex];
-}
-
-
-void mainGame() {
-    printf("Starting the game...\n");
-    srand(time(NULL));
-
-    char *computerChoice = randomChoice();
-    char *userChoice = myChoice();
-
-    printf("Computer's choice: %s\n", computerChoice);
-    printf("Your choice: %s\n", userChoice);
-
-    if (strcmp(computerChoice, userChoice) == 0) {
-        printf("It's a tie!\n");
-    } else if ((strcmp(computerChoice, "rock") == 0 && strcmp(userChoice, "scissors") == 0) ||
-               (strcmp(computerChoice, "paper") == 0 && strcmp(userChoice, "rock") == 0) ||
-               (strcmp(computerChoice, "scissors") == 0 && strcmp(userChoice, "paper") == 0)) {
-        printf("Computer wins!\n");
-    } else {
-        printf("You win!\n");
-    }
-}
-
 int main() {
     int choice;
 
@@ -77,7 +18,56 @@ int main() {
 
         switch (choice) {
             case 1:
-                mainGame();
+                printf("Starting the game...\n");
+                srand(time(NULL));
+
+                // Define the choices as strings
+                char *choices[] = {"rock", "paper", "scissors"};
+                int num_choices = 3; // Number of choices
+
+                int randomIndex = rand() % num_choices;
+                char *randomChoice = choices[randomIndex];
+
+                int user_choice;
+                char *userChoice;
+
+                while (1) {
+                    printf("Enter your choice:\n1. Rock\n2. Paper\n3. Scissors\n");
+                    printf("Your choice: ");
+                    scanf("%d", &user_choice);
+
+                    if (user_choice >= 1 && user_choice <= 3) {
+                        break; // Exit the loop when a valid choice is entered
+                    } else {
+                        printf("Invalid choice. Please choose a number between 1 and 3.\n");
+                    }
+                }
+
+                switch (user_choice) {
+                    case 1:
+                        userChoice = "rock";
+                        break;
+                    case 2:
+                        userChoice = "paper";
+                        break;
+                    case 3:
+                        userChoice = "scissors";
+                        break;
+                }
+
+                printf("Computer's choice: %s\n", randomChoice);
+                printf("Your choice: %s\n", userChoice);
+
+                if (strcmp(randomChoice, userChoice) == 0) {
+                    printf("It's a tie!\n");
+                } else if ((strcmp(randomChoice, "rock") == 0 && strcmp(userChoice, "scissors") == 0) ||
+                           (strcmp(randomChoice, "paper") == 0 && strcmp(userChoice, "rock") == 0) ||
+                           (strcmp(randomChoice, "scissors") == 0 && strcmp(userChoice, "paper") == 0)) {
+                    printf("Computer wins!\n");
+                } else {
+                    printf("You win!\n");
+                }
+
                 break;
             case 2:
                 printf("Exiting the game. Goodbye!\n");
@@ -85,8 +75,9 @@ int main() {
             case 3:
                 printf("Development Screen\n");
                 printf("Made by Aadit 41\n");
-                printf("Made by Chirag 37\n");
-                printf("Made by Kunal 48\n");
+                printf("Contributors:\n");
+                printf("- Kunal 48\n");
+                printf("- Chirag 37\n");
                 break;
             default:
                 printf("Invalid choice. Please select a valid option (1-3 or 0 to exit).\n");
